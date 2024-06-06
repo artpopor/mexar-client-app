afa613fbb76ee56152934c087ea8892fe1ff3271
artpopor <artpopor@gmail.com>
2024-06-06 11:38:54 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-white'>hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+
+    "./src/*.{tsx,ts,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
